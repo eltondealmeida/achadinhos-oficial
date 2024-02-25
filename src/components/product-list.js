51ef72090e657909539f63a8c99b297f4866b180
eltondealmeida <elton_de_almeida@hotmail.com>
2024-02-24 23:56:19 -0300

    Insert: Novos produtos.

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -1,16 +1,50 @@
 export const productList = [
-  { id: 1, name: 'Dispensador Líquido Sabonete', imageUrl: 'https://down-br.img.susercontent.com/file/br-50009109-acacb32f2bc4b5b48bc5a4b9bf36da3e', productUrl: 'https://shope.ee/50B48JhmOP' },
-  { id: 2, name: '2', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 3, name: '3', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 4, name: '4', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 5, name: '5', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 6, name: '6', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 7, name: '7', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 8, name: '8', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 9, name: '9', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 10, name: '10', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 11, name: '11', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 12, name: '12', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 13, name: '13', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
-  { id: 14, name: '14', imageUrl: 'produto.jpg', productUrl: 'https://exemplo.com/produto2' },
+  { 
+  id: 1, 
+  name: 'Dispensador Líquido Sabonete', 
+  imageUrl: 'https://down-br.img.susercontent.com/file/5ab7d2e435c175ddafa2c78a653e8bb3', 
+  productUrl: 'https://shope.ee/50B48JhmOP' 
+  },
+  { 
+  id: 2, 
+  name: 'Rack de Armazenamento de 360 Rotativa', 
+  imageUrl: 'https://down-br.img.susercontent.com/file/sg-11134201-23030-638haub9koov08', 
+  productUrl: 'https://shope.ee/9zZk5wvprn' 
+  },
+  { 
+  id: 3, 
+  name: 'Protetor De Silicone Para Ralo', 
+  imageUrl: 'https://down-br.img.susercontent.com/file/c521e97686165f4bce305c1914c3d213', 
+  productUrl: 'https://shope.ee/2fn9NFhgaf' 
+  },
+  { 
+    id: 4, 
+    name: 'Tapete Emborrachado Mágico De Banheiro Absorvente Antiderrapante', 
+    imageUrl: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lkk74vyi1cvc30', 
+    productUrl: 'https://shope.ee/3ffgZQQzq5' 
+  },
+  { 
+      id: 5, 
+      name: 'Organizador De Banheiro Gaveta Nórdica Prateleira Plástico', 
+      imageUrl: 'https://down-br.img.susercontent.com/file/sg-11134201-23020-piuvzzhsmynvdd', 
+      productUrl: 'https://shope.ee/3ffgcxMGQx' 
+  },
+  { 
+    id: 6, 
+    name: 'Lixeira Suspensa Sem Furos', 
+    imageUrl: 'https://down-br.img.susercontent.com/file/br-11134207-7r98s-lmhp39q6r29j15', 
+    productUrl: 'https://shope.ee/1VbC3jBSSq' 
+  },
+  { 
+    id: 7, 
+    name: 'Escova Sanitária Quadrada Limpeza Banheiro Parede Silicone', 
+    imageUrl: 'https://down-br.img.susercontent.com/file/br-11134201-23020-6cidnixmo1nv15', 
+    productUrl: 'https://shope.ee/5pkBEOmC3Q' 
+  },
+  { 
+    id: 8, 
+    name: 'Extensor De Torneiras Universal Aerador Giratório Splash', 
+    imageUrl: 'https://down-br.img.susercontent.com/file/sg-11134201-7rbmt-lpi0txmpmbai4a', 
+    productUrl: 'https://shope.ee/3ffgej4ECn' 
+  },
 ];
